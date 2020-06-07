da8ab8498c287b91ba130f42f79815de8e2a03c7
Aaron Smith <smitham50@gmail.com>
2020-06-07 11:46:07 -0400

    added save to file function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 func main() {
+	// cards := newDeck()
+
+	// hand, remainingDeck := deal(cards, 5)
+
+	// hand.print()
+	// remainingDeck.print()
+
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	cards.saveToFile("my_cards")
 }
