07eaf9dfef69b073f6d14a023735bd25805d6bfc
Aaron Smith <smitham50@gmail.com>
2020-06-07 12:30:25 -0400

    added shuffle function

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 5)
-
-	// hand.print()
-	// remainingDeck.print()
-
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 
 	cards.print()
 }
